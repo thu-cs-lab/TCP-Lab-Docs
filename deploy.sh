6f020f8ff8ce696e6decd96b67fabb96b3722c70
Jiajie Chen <c@jia.je>
2026-09-25 15:03:47 +0800

    Migrate to zensical

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ sleep 1
 git fetch
 git reset --hard origin/master
 poetry install
-poetry run mkdocs build
+poetry run zensical build
 mkdir -p /srv/deploy/tcp-lab-docs
 cp -r site/* /srv/deploy/tcp-lab-docs/
